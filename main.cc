faae129b1253403e241e72d37299b4e0c1c59117
Alexander Schoenau <0663779@my.scccd.edu>
2017-05-10 08:20:06 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <algorithm>
+#include <cstdlib>
 using namespace std;
+
+
+
 int main()
 {
-cout<<"Hello, World!"<<endl;
+
+int operand = 0;
+string operation;
+
+while (cin){
+
+	cin >> ws; // eat any whitespace
+
+int c = cin.peek();
+
+if(c == EOF) return 0; // if it's the end of file, quit.
+
+if(!isdigit(c)){ // is the next input a digit or not
+
+	cin >> operation;
+
+	if(operation == "QUIT" || operation == "quit") return 0;
+
+}
+
+else{
+
+	cin >> operand;
+
+
+}
+
+
+
+
+	}
 }
